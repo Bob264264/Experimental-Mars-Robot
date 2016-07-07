cd12d2b267a31f7a7bc19a2f5e2ebd684f34394d
William <superwilliamli@yahoo.com>
2016-07-07 13:07:10 -0400

    Fixed Offset

--- electronics/Actuator_Retract.ino
+++ electronics/Actuator_Retract.ino
@@ -12,17 +12,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   //45 = retract fully
   //135 = extend fully
-  int pos;
+  float pos;
 
   if (Serial.available() > 0){
     pos = Serial.parseInt();
-    if (pos < 0 || pos > 138){
-      Serial.print("Extension length out of bounds.");
-      break;
-    }
     Serial.print("Extending to ");
     Serial.print(pos);
     Serial.println("mm");
-    act.write(map(pos, 0, 138, 45, 135));
+    float out = 0.8996*pos + 0.5874;
+    Serial.println(out);
+    pos = 1.1109*pos - 0.6051;
+//    act.write(map(pos, 0.5874, 126.53, 45, 135));
+    act.write(map(pos, 0, 140, 45, 135));
+//    act.write(90*((14*pos/9)-40)/140 + 45);
   }
 }
