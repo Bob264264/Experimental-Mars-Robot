2c90e87fb45af7cebd692188ff587f3577482f00
Abhinav Raghunathan <abhinavr2121@gmail.com>
2016-07-11 19:51:35 -0400

    Color Sensing

--- interface/recognition/detect_color.py
+++ interface/recognition/detect_color.py
@@ -4,35 +4,48 @@
 # import the necessary packages
 import numpy as np
 import argparse
-import cv
+import cv2
 
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
-args = vars(ap.parse_args())
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", help = "path to the image")
+#args = vars(ap.parse_args())
 
 # load the image
-image = cv2.imread(args["image"])
+#image = cv2.imread(args["image"])
 
 # define the list of boundaries
-boundaries = [
-	([17, 15, 100], [50, 56, 200]),
-	([86, 31, 4], [220, 88, 50]),
-	([25, 146, 190], [62, 174, 250]),
-	([103, 86, 65], [145, 133, 128])
-]
 
-# loop over the boundaries
-for (lower, upper) in boundaries:
-	# create NumPy arrays from the boundaries
-	lower = np.array(lower, dtype = "uint8")
-	upper = np.array(upper, dtype = "uint8")
+cap = cv2.VideoCapture(0)
+cap.open(0)
+print cap
+while(True):
+	ret, frame = cap.read(cap.grab())
+	if frame.empty():
+		break
+	#boundaries = [
+	#	([200, 200, 200], [255, 255, 255])
+	#]
 
-	# find the colors within the specified boundaries and apply
-	# the mask
-	mask = cv2.inRange(image, lower, upper)
-	output = cv2.bitwise_and(image, image, mask = mask)
+	# loop over the boundaries
+	#for (lower, upper) in boundaries:
+		# create NumPy arrays from the boundaries
+	#	lower = np.array(lower, dtype = "uint8")
+	#	upper = np.array(upper, dtype = "uint8")
 
-	# show the images
-	cv2.imshow("images", np.hstack([image, output]))
-	cv2.waitKey(0)
+		# find the colors within the specified boundaries and apply
+		# the mask
+	#	mask = cv2.inRange(image, lower, upper)
+	#	output = cv2.bitwise_and(image, image, mask = mask)
+
+		# show the images
+	#	cv2.imshow("images", np.hstack([image, output]))
+	#	cv2.waitKey(0)
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	cv2.imshow('frame', gray)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
