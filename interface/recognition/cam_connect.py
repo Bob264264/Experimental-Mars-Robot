ead7cfd03d6145835f6833211383ca189e19d753
Abhinav Raghunathan <abhinavr2121@gmail.com>
2016-07-11 20:10:05 -0400

    Exit mechanism improved

--- interface/recognition/cam_connect.py
+++ interface/recognition/cam_connect.py
@@ -4,7 +4,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while True:
-	ret, frame = cap.read()
+	frame = cap.read()
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	boundaries = [
 		([200, 200, 200], [255, 255, 255])
@@ -19,11 +19,12 @@ while True:
 		# find the colors within the specified boundaries and apply
 		# the mask
 		mask = cv2.inRange(frame, lower, upper)
-		#output = cv2.bitwise_and(frame, frame, mask = mask)
-		output = cv2.bitwise_and(frame, mask = mask)
+		output = cv2.bitwise_and(frame, frame, mask = mask)
+
 		# show the images
 		cv2.imshow("frame", np.hstack([frame, output]))
-		if cv2.waitKey(1) & 0xFF == ord('q'):
+		if cv2.waitKey(0) & 0xFF == ord('q'):
 			break
+
 cap.release()
 cv2.destroyAllWindows()
