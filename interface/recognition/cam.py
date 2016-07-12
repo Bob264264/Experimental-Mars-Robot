1a8e98aa7741a633751cf0e723859324f48bcfa4
Abhinav Raghunathan <abhinavr2121@gmail.com>
2016-07-11 20:13:48 -0400

    Frame resize

---
+++ interface/recognition/cam.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = cap.read()
+	boundaries = [
+		([200, 200, 200], [255, 255, 255])
+	]
+
+	# loop over the boundaries
+	for (lower, upper) in boundaries:
+		# create NumPy arrays from the boundaries
+		lower = np.array(lower, dtype = "uint8")
+		upper = np.array(upper, dtype = "uint8")
+
+		# find the colors within the specified boundaries and apply
+		# the mask
+		mask = cv2.inRange(frame, lower, upper)
+		output = cv2.bitwise_and(frame, frame, mask = mask)
+
+		# show the images
+		cv2.imshow("frame", np.hstack([frame, output]), "WINDOW_NORMAL")
+		if cv2.waitKey(0) & 0xFF == ord('q'):
+			break
+
+cap.release()
+cv2.destroyAllWindows()
