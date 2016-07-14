d779dcbc16472e42635e64b3998549b0fe2ca83b
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-07-14 00:56:01 -0400

    Added GUI control buttons

--- interface/api.py
+++
@@ -1,9 +0,0 @@
-class RobotController():
-	def moveactuator(actuatornum, amount):\
-		pass
-	def rotatewheel(wheelnum, amount):
-		pass
-	def movearm(armnum, amount):
-		pass
-	def movemass(armnum, amount):
-		pass
--- interface/web_interface/webgui.py
+++ interface/web_interface/webgui.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from api import RobotController
-from multiprocessing.dummy import Pool
 rc = RobotController()
 
 @app.route("/", methods=["GET"])
@@ -11,15 +10,28 @@ def controlpage():
 
 @app.route("/", methods=["POST"])
 def controlapi():
-	jsonrequest = json.loads(request.form("jsonrequest"))
-	for actuator in jsonrequest["actuators"]:
-		Pool(processes=1).pool.apply_async(rc.moveactuator, [actuator[0], actuator[1]]) #Actuator number, actuator amount (float)
-	for wheelmotor in jsonrequest["wheelmotors"]:
-		Pool(processes=1).pool.apply_async(rc.rotatewheel, [wheelmotor[0], wheelmotor[1]])
-	for armmotor in jsonrequest["armmotors"]:
-		Pool(processes=1).pool.apply_async(rc.movearm, [armmotor[0], armmotor[1]])
-	for pulleymotor in jsonrequest["armmotors"]:
-		Pool(processes=1).pool.apply_async(rc.movemass, [pulleymotor[0], pulleymotor[1]])
+	try:
+		jsonrequest = json.loads(request.form("jsonrequest"))
+		if not jsonrequest["automanuever"]:
+			assert jsonrequest["actuators"]
+			assert jsonrequest["wheelmotors"]
+			assert jsonrequest["armmotors"]
+			assert jsonrequest["pulleymotors"]
+
+			#Pool is for asynchronous actuation
+			for actuator in jsonrequest["actuators"]:
+				Pool(processes=1).pool.apply_async(rc.moveactuator, [actuator[0], actuator[1]]) #Actuator number, actuator amount (float)
+			for wheelmotor in jsonrequest["wheelmotors"]:
+				Pool(processes=1).pool.apply_async(rc.rotatewheel, [wheelmotor[0], wheelmotor[1]])
+			for armmotor in jsonrequest["armmotors"]:
+				Pool(processes=1).pool.apply_async(rc.movearm, [armmotor[0], armmotor[1]])
+			for pulleymotor in jsonrequest["pulleymotors"]:
+				Pool(processes=1).pool.apply_async(rc.movemass, [pulleymotor[0], pulleymotor[1]])
+	except:
+		return "invalid request"
+
+
+	return "request completed"
 
 if __name__ == "__main__":
     app.run()
