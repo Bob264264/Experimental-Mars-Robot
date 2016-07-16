2de762de4d42c10d502b6a70a622ccd159a4341b
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2016-07-16 16:11:41 -0400

    Added paper

---
+++ interface/web_interface/api.py
@@ -0,0 +1,9 @@
+class RobotController():
+	def moveactuator(actuatornum, amount):
+		pass
+	def rotatewheel(wheelnum, amount):
+		pass
+	def movearm(armnum, amount):
+		pass
+	def movemass(armnum, amount):
+		pass
